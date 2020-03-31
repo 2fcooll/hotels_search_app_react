ffb82458b0f980f3b5125498afa2af96ec901e27
Maks <mogachev.m@nicecode.biz>
2020-03-31 19:41:25 +0300

    .

--- src/App.js
+++ src/App.js
@@ -6,7 +6,17 @@ import Table from './components/table'
 
 class App extends Component {
   state = {
-    hotels: []
+    hotels: [],
+    hotelCountry: '',
+    hotelType: '',
+    hotelStarsNumber: '',
+    hotelRewiewsFrom: '',
+    hotelPriceTo: '',
+    hotelOneStar: false,
+    hotelTwoStar: false,
+    hotelThreeStar: false,
+    hotelFourStar: false,
+    hotelFiveStar: false
   }
 
   componentDidMount() {
@@ -19,58 +29,100 @@ class App extends Component {
     })
   }
 
+  handleChange = (field, value) => {
+    this.setState({ [field]: value })
+  }
+
   render() {
     return (
       <div>
         <Button title={'Очистить фильтры'} />
         <div>
           <label>Страна</label>
-          <Select options={['Амстердам', 'Вена', 'Прага']} />
+          <Select
+            getValue={value => this.handleChange('hotelCountry', value)}
+            options={['Амстердам', 'Вена', 'Прага']}
+          />
         </div>
         <div>
           <label>Тип</label>
-          <Select options={['Отель', 'Апартаменты']} disabledSearch multiple />
+          <Select
+            getValue={value => this.handleChange('hotelType', value)}
+            options={['Отель', 'Апартаменты']}
+            disabledSearch
+            multiple
+          />
         </div>
         <div>
           <label>Звёзды</label>
           <div>
             <label>
-              <input type='checkbox' />
+              <input
+                type='checkbox'
+                checked={this.state.hotelOneStar}
+                onChange={e => this.handleChange('hotelOneStar', e.target.checked)}
+              />
               1 звезда
             </label>
           </div>
           <div>
             <label>
-              <input type='checkbox' />
+              <input
+                type='checkbox'
+                checked={this.state.hotelTwoStar}
+                onChange={e => this.handleChange('hotelTwoStar', e.target.checked)}
+               />
               2 звезда
             </label>
           </div>
           <div>
             <label>
-              <input type='checkbox' />
+              <input
+                type='checkbox'
+                checked={this.state.hotelThreeStar}
+                onChange={e => this.handleChange('hotelThreeStar', e.target.checked)}
+               />
               3 звезда
             </label>
           </div>
           <div>
             <label>
-              <input type='checkbox' />
+              <input
+                type='checkbox'
+                checked={this.state.hotelFourStar}
+                onChange={e => this.handleChange('hotelFourStar', e.target.checked)}
+               />
               4 звезда
             </label>
           </div>
           <div>
             <label>
-              <input type='checkbox' />
+              <input
+                type='checkbox'
+                checked={this.state.hotelFiveStar}
+                onChange={e => this.handleChange('hotelFiveStar', e.target.checked)}
+               />
               5 звезда
             </label>
           </div>
         </div>
         <div>
           <label>Количество отзывов от</label>
-          <input type='text' />
+          <input
+            type='text'
+            value={this.state.hotelRewiewsFrom}
+            onChange={e => this.handleChange('hotelRewiewsFrom', e.target.value)}
+          />
         </div>
         <div>
           <label>Цена до</label>
-          <input type='range' min='0' max='100500' />
+          <input
+            type='range'
+            min='0'
+            max='100500'
+            value={this.state.hotelPriceTo}
+            onChange={e => this.handleChange('hotelPriceTo', e.target.value)}
+          />
         </div>
         <Button title={'Применить фильтры'} />
         <div>
